752631e01c3fe87dc80256747b560d6bd5e23862
Aris <arisamiga@gmail.com>
2021-06-29 22:48:42 +0100

    Refractor code + capped tickets at 1

--- commands/startapps.js
+++ commands/startapps.js
@@ -1,85 +1,96 @@
 const Jsonfile = require('../config.json');
 exports.run = async (client, message, args) => {
-if (message.author.id !== Jsonfile.owner) return message.channel.send("Sorry but you cant use this command D:").then((msg) => {
+  if (message.author.id !== Jsonfile.owner) return message.channel.send("Sorry but you cant use this command D:").then((msg) => {
     setTimeout(() => msg.delete(), 7000);
-})
-    const signup = await message.channel.send({embed: {
-        color: Jsonfile.signup_color,
+  })
+  const signup = await message.channel.send({
+    embed: {
+      color: Jsonfile.signup_color,
+      fields: [{
+        name: Jsonfile.signup_title,
+        value: "To create a ticket react with  📩"
+      }],
+      timestamp: new Date(),
+      footer: {
+        icon_url: client.user.avatarURL()
+      }
+    }
+  });
+  await signup.react("📩")
+  const collector = signup.createReactionCollector(
+    (reaction, user) => message.guild.members.cache.find((member) => member.id === user.id), {
+      dispose: true
+    }
+  );
+  collector.on("collect", (reaction, user) => {
+    switch (reaction.emoji.name) {
+      case "📩":
+        let channelname = `ticket-${user.username}-${user.discriminator}`
+        channelname = channelname.replace(/\s/g, '-').toLowerCase()
+        if (message.guild.channels.cache.find(channel => channel.name === channelname)) { //checks if there in an item in the channels collection that corresponds with the supplied parameters, returns a boolean
+          user.send(`You already have an ongoing ticket.`).catch(console.error);
+          reaction.users.remove(user.id)
+          return; //prevents the rest of the code from being executed
+        } else {
+          contining(user)
+        }
+        break;
+    }
+  })
+  async function contining(user) {
+    const channel = await message.guild.channels.create(`ticket: ${user.username + "-" + user.discriminator}`);
+
+    channel.updateOverwrite(message.guild.id, {
+      "SEND_MESSAGE": false,
+      "VIEW_CHANNEL": false,
+    });
+    channel.updateOverwrite(user.id, {
+      "SEND_MESSAGE": true,
+      "VIEW_CHANNEL": true,
+    });
+    channel.updateOverwrite(Jsonfile.Channelrole, {
+      "SEND_MESSAGE": true,
+      "VIEW_CHANNEL": true,
+    })
+
+    const reactionMessage = await channel.send({
+      embed: {
+        color: Jsonfile.answer_color,
         fields: [{
-            name: Jsonfile.signup_title,
-            value: "To create a ticket react with  📩"
-          }
-        ],
+          name: Jsonfile.answer_title,
+          value: Jsonfile.answer_description
+        }],
         timestamp: new Date(),
         footer: {
           icon_url: client.user.avatarURL()
         }
       }
-    });
-    await signup.react("📩")
-    const collector = signup.createReactionCollector(
-        (reaction, user) => message.guild.members.cache.find((member) => member.id === user.id),
-        { dispose: true }
-        );
-    collector.on("collect", (reaction, user) => {
-        switch (reaction.emoji.name) {
-            case "📩":
-                contining(user)
-                reaction.users.remove(user.id)
-            break;
-        }
     })
-    async function contining(user){
-        const channel = await message.guild.channels.create(`ticket: ${user.username + "-" + user.discriminator}`);
+    await reactionMessage.react("🔒");
+    await reactionMessage.react("⛔");
 
-        channel.updateOverwrite(message.guild.id, {
-        "SEND_MESSAGE": false,
-        "VIEW_CHANNEL": false,
-        });
-        channel.updateOverwrite(user.id, {
-        "SEND_MESSAGE": true,
-        "VIEW_CHANNEL": true,
-        });
-        channel.updateOverwrite(Jsonfile.Channelrole,{
-        "SEND_MESSAGE": true,
-        "VIEW_CHANNEL": true,
-        })
+    const collector = reactionMessage.createReactionCollector(
+      (reaction, user) => message.guild.members.cache.find((member) => member.id === user.id).hasPermission("ADMINISTRATOR") || message.guild.members.cache.find((member) => member.id === user.id).roles.cache.find(r => r.id === Jsonfile.Channelrole), {
+        dispose: true
+      }
+    );
 
-        const reactionMessage = await channel.send({embed: {
-            color: Jsonfile.answer_color,
-            fields: [{
-                name: Jsonfile.answer_title,
-                value: Jsonfile.answer_description
-              }
-            ],
-            timestamp: new Date(),
-            footer: {
-              icon_url: client.user.avatarURL()
-            }
+
+    collector.on("collect", (reaction, user) => {
+      switch (reaction.emoji.name) {
+        case "🔒":
+          channel.updateOverwrite(user.id, {
+            "SEND_MESSAGES": false
+          });
+          break;
+        case "⛔":
+          if (message.guild.channels.cache.find(c => c.name.toLowerCase() === channel.name)) { //checks if there in an item in the channels collection that corresponds with the supplied parameters, returns a boolean
+            setTimeout(() => channel.delete(), 5000);
+            channel.send("Deleting this channel in 5 seconds!");
+            return;
           }
-        })
-        await reactionMessage.react("🔒");
-        await reactionMessage.react("⛔");
-
-        const collector = reactionMessage.createReactionCollector(
-        (reaction, user) => message.guild.members.cache.find((member) => member.id === user.id).hasPermission("ADMINISTRATOR") || message.guild.members.cache.find((member) => member.id === user.id).roles.cache.find(r => r.id === Jsonfile.Channelrole),
-        { dispose: true }
-        );
-        
-
-        collector.on("collect", (reaction, user) => {
-        switch (reaction.emoji.name) {
-            case "🔒":
-            channel.updateOverwrite(user.id, { "SEND_MESSAGES": false });
-            break;
-            case "⛔":
-            if (message.guild.channels.cache.find(c => c.name.toLowerCase() === channel.name)) { //checks if there in an item in the channels collection that corresponds with the supplied parameters, returns a boolean
-                setTimeout(() => channel.delete(), 5000);
-                channel.send("Deleting this channel in 5 seconds!");
-                return;
-            }
-            break;
-        }
-        });
-    }
+          break;
+      }
+    });
   }
+}
